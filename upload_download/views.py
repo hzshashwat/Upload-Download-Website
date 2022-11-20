035939a6bd3ab4d08794a06503bb6d98ce910527
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-20 17:17:28 +0530

    view.py file configured

--- upload_download/views.py
+++ upload_download/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DocumentForm
 
 # Create your views here.
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'model_form_upload.html', {
+        'form': form
+    })
+
+def homeview(request):
+    return render(request, 'homepage.html')
