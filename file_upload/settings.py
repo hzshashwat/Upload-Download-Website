add39d8f0efe666f4cb86519ed978ef57d0075f5
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-20 17:26:53 +0530

    Minor bug resolved in settings.py file

--- file_upload/settings.py
+++ file_upload/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'upload_download'
+    'upload_download',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
