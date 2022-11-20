871b31704cae86cd47baab38aaecfb4023b16aa0
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-20 17:28:49 +0530

    Routing Configured and ran migrations

--- upload_download/urls.py
+++ upload_download/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import *
 
 urlpatterns = [
-    
+    path('', model_form_upload, name='upload'),
+    path('home', homeview, name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
